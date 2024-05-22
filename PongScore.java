9dba7f18e2e756456977c5eb7deee3d5fcb8bac0
kcavs <kcav1321@gmail.com>
2024-05-22 14:36:18 -0400

    hi

--- PongScore.java
+++ PongScore.java
@@ -1,3 +1,12 @@
 public class PongScore {
-    
+    int score;
+    public PongScore(){
+        score=0;
+    }
+    public int getScore(){
+      return score;  
+    }
+    public void scorePoints(int points){
+        score=(score+points);
+    }
 }

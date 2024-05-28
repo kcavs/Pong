ab421670efd43930d5bdf183dd796f126459c7ef
kcavs <kcav1321@gmail.com>
2024-05-28 14:33:09 -0400

    k

--- PongPaddle.java
+++ PongPaddle.java
@@ -1,3 +1,26 @@
 public class PongPaddle {
-    
+    private int x;
+    private int y;
+    private int width;
+    private int height;
+    public PongPaddle(int top, int left, int w, int h){
+        x=left;
+        y=top;
+        width=w;
+        height=h;
+    }
+    public int getBottomY(int b){
+        b=(y-height-1);
+        return b;
+    }
+    public int getTopY(){
+        return y;
+    }
+    public int getLeftX(){
+        return x;
+    }
+    public int getRightX(int r){
+        r=(x+width-1);
+        return x;
+    }
 }
